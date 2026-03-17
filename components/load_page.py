9cd370c283cdbbf852e637896c2f2cc7a70c9ef9
nagampere <nagayohei0508@gmail.com>
2026-03-17 17:11:47 +0900

    サイドバーの株式会社AMANEの紹介を更新し、フィードバックフォームを削除

--- components/load_page.py
+++ components/load_page.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-from components.save_memo import save_memo_to_motherduck
-
 # ページ設定を読み込む関数
 def load_page_config():
     # Streamlitのページ設定
@@ -23,34 +21,23 @@ def load_page_config():
 
     # サイドバーに株式会社AMANEの会社紹介を追加
     with st.sidebar.expander("株式会社AMANEについて", expanded=False):
-        st.markdown("""
-        **Mission**  
-        テクノロジーと現場、さまざまなステークホルダーをつなげ、価値がめぐる仕組みを創出し、豊かな都市生活を実現する
+        st.markdown(
+        """
+        **HP**
+
+        [https://amane.ltd/](https://amane.ltd/)
+                    
+        **所在地**
+
+        〒105-0012 
+        東京都港区芝大門一丁目2番14号
         
         **事業内容**
-        - [アーバンテック事業開発支援](https://amane.ltd/service/urbantech_business-development_support/)
-        - [アーバンテック自社開発](https://amane.ltd/service/urbantech_in-house-development/)
+        - アーバンテック事業開発支援
+        - アーバンテック自社開発
         
         [お問合せはこちら](https://amane.ltd/contact/)
         """)
-
-
-    # サイドバーにフィードバックフォームを追加
-    with st.sidebar.expander("ご意見・感想", expanded=False):
-        with st.form("sidebar_memo_form"):
-            memo = st.text_area(
-                "※送信された内容は開発チームで確認し、今後の改善に役立てます。",
-                height=250
-            )
-            save_clicked = st.form_submit_button("送信")
-        if save_clicked:
-            ok, msg = save_memo_to_motherduck(
-                memo_text=memo
-            )
-            if ok:
-                st.success(msg)
-            else:
-                st.error(msg)
 
     # サイドバーに権利表記を追加し、リンクを設定
     st.sidebar.markdown(
